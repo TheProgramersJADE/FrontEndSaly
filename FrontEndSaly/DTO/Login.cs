3f762dfe7225313dd0612af7b24076350e6e54d8
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-09-23 23:39:11 -0600

    Agregar archivos de proyecto.

---
+++ FrontEndSaly/DTO/Login.cs
@@ -0,0 +1,4 @@
+﻿namespace FrontEndSaly.DTO
+{
+    
+}
---
+++ FrontEndSaly/DTO/Users.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FrontEndSaly.DTO
+{
+    public class Users
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("role_id")]
+        public int Role_Id { get; set;}
+    }
+}
